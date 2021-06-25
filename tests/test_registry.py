5cab29555dbe764a4d153e08216f81fc2cc2eaf7
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-06-24 18:28:24 -0600

    Update registry tests (#10)

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+import catalogue
 import pytest
 
 import funnel
@@ -7,7 +8,11 @@ import funnel
 
 @pytest.mark.parametrize(
     'registry_name,func_name,expected',
-    [('serializers', 'xarray.netcdf', True), ('serializers', 'my_func', False)],
+    [
+        ('serializers', 'xarray.netcdf', True),
+        ('serializers', 'my_func', False),
+        ('my_registry', 'test', False),
+    ],
 )
 def test_has(registry_name, func_name, expected):
     assert funnel.registry.has(registry_name, func_name) == expected
@@ -18,3 +23,26 @@ def test_has(registry_name, func_name, expected):
 )
 def test_get(registry_name, func_name):
     assert isinstance(funnel.registry.get(registry_name, func_name), Callable)
+
+
+def test_get_error():
+    with pytest.raises(ValueError):
+        funnel.registry.get('my_registry', 'my_func')
+
+    with pytest.raises(catalogue.RegistryError):
+        funnel.registry.get('serializers', 'my_func')
+
+
+def test_create_error():
+    with pytest.raises(ValueError):
+        funnel.registry.create('serializers')
+
+
+def test_create():
+    funnel.registry.create('my_registry')
+
+    @funnel.registry.my_registry.register('test')
+    def my_func():
+        return
+
+    assert funnel.registry.has('my_registry', 'test')
