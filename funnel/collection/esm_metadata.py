5ed81d6e4751d9c66b5388866ee2d07cc3f6f784
Max Grover <mgroverwx@gmail.com>
2021-07-15 17:39:09 -0600

    add metadata store for collection

---
+++ funnel/collection/esm_metadata.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pydantic
+
+from funnel.metadata import BaseMetadataStore
+
+
+@pydantic.dataclasses.dataclass
+class EsmMetdataStore(BaseMetadataStore):
+    def __post_init_post_parse__(self):
+        self.df = pd.DataFrame(
+            columns=[
+                'key',
+                'serializer',
+                'dump_kwargs',
+                'load_kwargs',
+                'esm_collection_json',
+                'esm_collection_key',
+                'collection_name',
+                'variable',
+            ]
+        ).set_index('key')
+
+    def put(self, key, value, origins_dict={}, serializer='auto', **dump_kwargs):
+        # Writes, returns a dictionary used (what serializer? what dump kwargs)
+        receipt = self.cache_store.put(key, value, serializer, **dump_kwargs)
+        print(receipt)
+        receipt_dict = receipt.dict()
+        receipt_dict.update(origins_dict)
+        self.df.loc[key] = receipt_dict
+
+    def get(self, key, **load_kwargs):
+        # Gives you a pandas series
+        x = self.df.loc[key]
+
+        # If this is an empty dictionary, pick the non-empty one
+        load_kwargs = load_kwargs or x.load_kwargs
+
+        return self.cache_store.get(key, x.serializer, **load_kwargs)
