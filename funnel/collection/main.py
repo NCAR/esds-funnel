d7a88346f14e8cfbe7b974a2cd5cc5ede102190e
Max Grover <mgroverwx@gmail.com>
2021-08-02 15:52:07 -0600

    update logic of if else block

--- funnel/collection/main.py
+++ funnel/collection/main.py
@@ -56,61 +56,62 @@ class Collection:
         if isinstance(variable, str):
             variable = [variable]
 
-        if isinstance(variable, (list, tuple)):
-            for v in variable:
-                individual_query = self.esm_collection_query.copy()
+        if not isinstance(variable, (list, tuple)):
 
-                if (v not in self.base_variables) and (v in derived_variable_registry):
-                    self.origins_dict.update(derived_variable=True)
-                    derived_var = derived_variable_registry[v]
-                    individual_query['variable'] = derived_var.dependent_vars
-
-                elif v in self.base_variables:
-                    individual_query['variable'] = v
-
-                else:
-                    raise ValueError(f'{v} not found in base variables or derived variables')
-
-                # individual_query = self.esm_collection_query.copy()['variable'] = v
-                self.origins_dict.update(esm_collection_query=individual_query)
-                subset_catalog = self.catalog.search(**individual_query)
-                for catalog_key in subset_catalog.keys():
-                    self.origins_dict.update(esm_collection_key=catalog_key)
-                    individual_key = tokenize(self.origins_dict)
-                    if individual_key in self.metadata_store:
-                        dsets[catalog_key] = self.metadata_store.get(individual_key)
-                    else:
-                        # read in the dataset - add to the dataset dict
-                        df = subset_catalog[catalog_key]
-
-                        # Open the dataset using the prescribed key
-                        ds = df(**self.kwargs).to_dask()
-
-                        # If there are derived variables, calculate them
-                        if self.origins_dict['derived_variable']:
-                            ds = derived_var(ds)
-
-                        # Apply operators
-                        self.apply_operators(ds)
-
-                        # Add this dataset to the dictionary of datasets
-                        dsets[catalog_key] = ds
-
-                        # Add this to the database
-                        if ds.nbytes > 1000000:
-                            warnings.warn(
-                                f'Warning: Dataset you are saving is larger than 1 GB \n Total size:{ds.nbytes * 1e-6} GB'
-                            )
-                        self.metadata_store.put(
-                            individual_key,
-                            dsets[catalog_key],
-                            self.serializer,
-                            custom_fields=self.origins_dict,
-                        )
-        else:
             raise TypeError(
                 f'Found `variable` to be an {type(variable)} type. `variable` must be a string, list, or tuple'
             )
+
+        for v in variable:
+            individual_query = self.esm_collection_query.copy()
+
+            if (v not in self.base_variables) and (v in derived_variable_registry):
+                self.origins_dict.update(derived_variable=True)
+                derived_var = derived_variable_registry[v]
+                individual_query['variable'] = derived_var.dependent_vars
+
+            elif v in self.base_variables:
+                individual_query['variable'] = v
+
+            else:
+                raise ValueError(f'{v} not found in base variables or derived variables')
+
+            # individual_query = self.esm_collection_query.copy()['variable'] = v
+            self.origins_dict.update(esm_collection_query=individual_query)
+            subset_catalog = self.catalog.search(**individual_query)
+            for catalog_key in subset_catalog.keys():
+                self.origins_dict.update(esm_collection_key=catalog_key)
+                individual_key = tokenize(self.origins_dict)
+                if individual_key in self.metadata_store:
+                    dsets[catalog_key] = self.metadata_store.get(individual_key)
+                else:
+                    # read in the dataset - add to the dataset dict
+                    df = subset_catalog[catalog_key]
+
+                    # Open the dataset using the prescribed key
+                    ds = df(**self.kwargs).to_dask()
+
+                    # If there are derived variables, calculate them
+                    if self.origins_dict['derived_variable']:
+                        ds = derived_var(ds)
+
+                    # Apply operators
+                    self.apply_operators(ds)
+
+                    # Add this dataset to the dictionary of datasets
+                    dsets[catalog_key] = ds
+
+                    # Add this to the database
+                    if ds.nbytes > 1000000:
+                        warnings.warn(
+                            f'Warning: Dataset you are saving is larger than 1 GB \n Total size:{ds.nbytes * 1e-6} GB'
+                        )
+                    self.metadata_store.put(
+                        individual_key,
+                        dsets[catalog_key],
+                        self.serializer,
+                        custom_fields=self.origins_dict,
+                    )
 
         return dsets
 
