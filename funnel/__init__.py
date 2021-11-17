4260800ebc8790641aa6cbff604b97dabf978d8d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-11-17 13:37:43 -0700

    Remove collection/derived functionality (#41)

--- funnel/__init__.py
+++ funnel/__init__.py
@@ -4,7 +4,6 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
 from .cache import CacheStore
-from .collection.main import Collection
 from .config import config
 from .metadata_db.main import BaseMetadataStore, MemoryMetadataStore, SQLMetadataStore
 from .registry import registry
